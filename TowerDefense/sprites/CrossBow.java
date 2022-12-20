bd8c276b942a8d8a839019e2b86a6d0debfc068f
15032002 <15032002@L829WD210002.edu.cbe.ab.ca>
2022-12-20 14:01:27 -0700

    Crossbow

---
+++ TowerDefense/sprites/CrossBow.java
@@ -0,0 +1,58 @@
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class CrossBow extends Tower {
+
+
+	private static Image image;
+	private static Image[] rotatedImages = new Image[360];
+	private double width;
+	private double height;
+	private double currentAngle = 0;
+	private double centerX;
+	private double centerY;
+	private int range = 100;
+	
+
+	public CrossBow(double centerX, double centerY, double reloadTime, double range, String imageName) {
+		super(centerX, centerY, .5, 250, "res/crossbow.png"); // each increment of 50 is 1 tile
+		
+
+		try {
+			image = ImageIO.read(new File("res/trebuchet.png"));
+		} catch (IOException e) {
+			System.out.println(e.toString());
+		}
+		if (image != null) {
+			for (int i = 0; i < 360; i++) {
+				rotatedImages[i] = ImageRotator.rotate(image, i);
+			}
+			this.height = image.getHeight(null);
+			this.width = image.getWidth(null);
+		}
+		
+	}
+
+
+	public Image getImage() {
+		
+		try {
+			image = rotatedImages[Math.abs((int) currentAngle)];
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println(e);
+		}
+		return image;
+		
+		
+	}
+	
+	
+	public Image getImage(int angle) {
+		
+		return rotatedImages[angle];
+	}
+
+}
