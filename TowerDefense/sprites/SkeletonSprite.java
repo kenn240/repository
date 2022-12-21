b516af88bc8163bc877d8e2391bd97e9ef0c50fd
kenn240 <98987663+kenn240@users.noreply.github.com>
2022-12-21 12:46:17 -0700

    jonahsalvatore

--- TowerDefense/sprites/SkeletonSprite.java
+++ TowerDefense/sprites/SkeletonSprite.java
@@ -14,29 +14,6 @@ public class SkeletonSprite extends Enemy {
 
 	private static Image image;
 	
-	
-	
-	
-	
-	
-
-	// an example of an enumeration, which is a series of constants in a list. this
-	// restricts the potential values of a variable
-	// declared with that type to only those values within the set, thereby
-	// promoting both code safety and readability
-	/*
-	private Direction direction = Direction.UP;
-
-	private enum Direction {
-		DOWN(0), LEFT(1), UP(2), RIGHT(3);
-
-		private int value = 0;
-
-		private Direction(int value) {
-			this.value = value;
-		}
-	};
-	*/
 	public SkeletonSprite(double centerX, double centerY, int health) {
 		super(centerX, centerY, health, 200);
 		
